f1b4d5bbc8aa08a2b4bdc5da717efee723ee9fff
allangonzalez454 <allangonzalez454@gmail.com>
2026-05-09 12:00:12 -0600

    Clase 02

---
+++ Clase_02/main.py
@@ -0,0 +1,18 @@
+print("Hola mundo")
+nombre = "Allan" 
+print(nombre)
+edad:int = int(input("Cual es tu edad?"))
+
+anno_de_nacimiento = 2026 - edad
+print(anno_de_nacimiento)
+mayor_de_edad = edad >= 18
+print(mayor_de_edad)
+soy_yo = nombre == "allan" and edad == 22
+no_soy_yo = not(nombre == "allan" and edad == 22)
+soy_yo = nombre == "allan" and edad == 22
+quizas_soy_yo = nombre == "allan" or edad == 22
+
+ 
+print(soy_yo)
+print(no_soy_yo)
+print(quizas_soy_yo)
