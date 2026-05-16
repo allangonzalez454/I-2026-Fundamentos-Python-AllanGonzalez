b5594aff19514fe12577a8c4ebac9e88355350fd
allangonzalez454 <allangonzalez454@gmail.com>
2026-05-16 11:09:58 -0600

    Clase 02

--- Clase_02/main.py
+++ Clase_02/main.py
@@ -1,16 +1,24 @@
+#inicializacion de variables
 print("Hola mundo")
 nombre = "Allan" 
-print(nombre)
 edad:int = int(input("Cual es tu edad?"))
 
+#Calcular ano de nacimiento
 anno_de_nacimiento = 2026 - edad
+
+#Imprimir resultados
+print(nombre)
 print(anno_de_nacimiento)
+
+#Calcular si es mayor de edad
 mayor_de_edad = edad >= 18
 print(mayor_de_edad)
-soy_yo = nombre == "allan" and edad == 22
+
+#Inicializacion de variables booleanas
+soy_yo = Allan (nombre == "allan" and edad == 22)
 no_soy_yo = not(nombre == "allan" and edad == 22)
-soy_yo = nombre == "allan" and edad == 22
-quizas_soy_yo = nombre == "allan" or edad == 22
+soy_yo = (nombre == "allan" and edad == 22)
+quizas_soy_yo = (nombre == "allan" or edad == 22)
 
  
 print(soy_yo)
