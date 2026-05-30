28d0fed4a85692473815e97047754b992b17aa90
allangonzalez454 <allangonzalez454@gmail.com>
2026-05-30 11:59:52 -0600

    Prueba 1

---
+++ Prueba 1/Control_inventario.py
@@ -0,0 +1,26 @@
+print("Ingrese la cantidad de productos que desea agregar al inventario")
+Productos = int(input("Ingrese la cantidad de productos: "))
+ 
+while Productos > 0: 
+print("Nombre de los productos")
+    
+Nombre_productos = input("Ingrese el nombre del producto: ") 
+
+Precio = int(input("Ingrese el precio del producto: "))
+  
+Cantidad = int(input("Ingrese la cantidad disponible: "))
+        
+        
+Valor_total = Precio * Cantidad 
+print(f"El valor total del inventario es: {Valor_total}")
+
+                                      
+    
+    
+    
+    
+    
+    
+    
+    
+     
